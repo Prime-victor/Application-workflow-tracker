4387b406d82aad5d024ad400af512c3e82539121
Prime-victor <victorkiptum85@gmail.com>
2026-05-20 13:23:14 +0300

    chore: scaffold django backend project

---
+++ Backend/manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+def main() -> None:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    if current_dir not in sys.path:
+        sys.path.insert(0, current_dir)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
